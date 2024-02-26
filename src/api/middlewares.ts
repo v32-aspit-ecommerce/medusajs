1cacaa02816731f035bb3a69a38336b20746b2a9
GARZTHONGK <albert.bruun.thomsen@gmail.com>
2024-02-26 09:55:36 +0100

    Fix broken middleware, ready to merge with main.

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -5,23 +5,18 @@ import {
   MedusaResponse,
   PublishableApiKey
 } from "@medusajs/medusa"
-import { MedusaContext } from "@medusajs/utils";
-// import ApiKeyMiddleware from '../services/api-key-service';
-import { EntityManager, Repository } from "typeorm";
+import { EntityManager } from "typeorm";
+
 const apiKeyMiddleware = async (
   req: MedusaRequest,
   res: MedusaResponse,
   next: MedusaNextFunction) => {
     const apiKey = req.headers["x-api-key"].toString();
-    console.log(apiKey)
     if (!apiKey){
       return res.status(400).json({ error: "Missing API key in header", missing_field: "x-api-key" });
     }
-    console.log("Creating manager")
     const manager: EntityManager = req.scope.resolve("manager");
-    console.log("Manager created")
     const publishableApiKeyRepository = manager.getRepository(PublishableApiKey);
-    console.log("getting record")
     const apiKeyRecord = await publishableApiKeyRepository.findOne({ where: { id: apiKey } });
     if (apiKeyRecord == null) {
       return res.status(401).json({ error: "Invalid API key"}); 
--- src/repositories/publishable_api_key.ts
+++
@@ -1,8 +0,0 @@
-import { dataSource } from "@medusajs/medusa/dist/loaders/database";
-import { PublishableApiKey } from '../models/PublishableApiKey';
-
- 
-
-const PublishableApiKeyRepository = dataSource.getRepository(PublishableApiKey);
-
-export default PublishableApiKeyRepository;
